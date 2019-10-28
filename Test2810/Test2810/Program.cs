23ffd50bf0fa12491e1b8284d6e0a19ead28d568
heoden <hothitouyen@gmail.com>
2019-10-28 15:37:21 +0800

    string

---
+++ Test2810/Test2810/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2810
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Bill";
+            string lastName = "Clinton";
+            Console.WriteLine($"my friend name is {firstName} and {lastName}");
+
+            Console.WriteLine($"First name has {firstName.Length} letters.");
+            Console.WriteLine($"Last name has {lastName.Length} letters.");
+        }
+    }
+}
